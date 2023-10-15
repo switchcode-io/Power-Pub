0863677a83b3d5ad26da4226fcc6e608e5819ada
G <5680013+gina305@users.noreply.github.com>
2023-10-15 23:33:23 +0000

    testing api key in tegration

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+    const TINY_KEY = process.env.TINY_KEY;
+
+    res.render('index',{ TINY_KEY });
+
+});
